697da39df0c48bfd18ac6e910c137f7835117cc8
neohum <neohum77@gmail.com>
2020-08-13 17:21:01 +0900

    chapter07

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import axios from 'axios';
+import Movie from './Movie';
+import './App.css';
 
 class App extends React.Component {
   state = {
@@ -12,7 +14,7 @@ class App extends React.Component {
       data: {
        data: { movies },
       },
-    } = await axios.get('https://yts-proxy.now.sh/list_movies.json');
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
     this.setState({ movies, isLoading: false });
   }
 
@@ -20,8 +22,29 @@ class App extends React.Component {
     this.getMovies();
   }
   render() {
-    const { isLoading } = this.state;
-    return <div>{isLoading ? 'Loading...' : 'We are ready'}</div>;
+    const { isLoading, movies } = this.state;
+    return <section className ="container">
+      {isLoading 
+      ? (<div className ="loader">
+        <span className="loader__text">Loading...</span> 
+        </div>
+        )
+      : (<div className="moviers">
+        {movies.map((movie) => {
+        return (
+        <Movie 
+          key={movie.id}
+          id={movie.id}
+          year = {movie.year}
+          title = {movie.title}
+          summary={movie.summary}
+          poster = { movie.medium_cover_image}
+          genres = {movie.genres}
+        />);
+      })}
+        </div>
+      )}
+    </section>;
   }
 }
 
